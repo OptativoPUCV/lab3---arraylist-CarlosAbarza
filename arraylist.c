772355280271637b9b3fe0c282ef62e66838ece3
Replit user <>
2023-03-29 22:11:37 +0000

    new try

--- arraylist.c
+++ arraylist.c
@@ -43,7 +43,7 @@ void push(ArrayList * l, void * data, int i){
     }
     l->capacity *= 2;
   }
-  for (int j = l->size - 1; j > i; j--) {
+  for (int j = l->size; j > i; j--) {
     l->data[j] = l->data[j-1];
   }
   l->data[i] = data;
